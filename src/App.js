337b4ac63026ae901c42aa9b8457def8287d81af
Nadia <nadia.mohamed89@gmail.com>
2019-06-26 17:31:15 +0100

    Add all pages components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,22 +9,32 @@ import Cart from './pages/CartPage';
 import Contact from './pages/ContactPage';
 import Products from './pages/ProductsPage';
 import SingleProduct from './pages/SingleProductPage';
-import Default from './pages/DefaultPage';
+import NotFound from './pages/NotFoundPage';
+
+import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+import SideCart from './components/SideCart';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <>
-        {/*Navbar, Sidebar, Cart, Fotter */}
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/contact" component={Contact} />
-          <Route exact path="/products" component={Products} />
-          <Route path="/products/:id" component={SingleProduct} />
-          <Route component={Default} />
-        </Switch>
+        <Router>
+          <Navbar />
+          <SideBar />
+          <SideCart />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/contact" component={Contact} />
+            <Route exact path="/products" component={Products} />
+            <Route path="/products/:id" component={SingleProduct} />
+            <Route component={NotFound} />
+            <Footer />
+          </Switch>
+        </Router>
       </>
     )
   }
